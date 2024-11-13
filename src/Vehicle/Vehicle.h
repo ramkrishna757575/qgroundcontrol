5b54bcfb8cfa44c747373e881007a48d9d5d3ea0
davidsastresas <davidsastresas@gmail.com>
2024-11-14 00:16:06 +0100

    Vehicle: add support for multi-GCS mavlink protocol

--- src/Vehicle/Vehicle.h
+++ src/Vehicle/Vehicle.h
@@ -971,6 +971,7 @@ private:
 #endif
     void _handleCameraImageCaptured     (const mavlink_message_t& message);
     void _handleADSBVehicle             (const mavlink_message_t& message);
+    void _handleCommandLong             (const mavlink_message_t& message);
     void _missionManagerError           (int errorCode, const QString& errorMsg);
     void _geoFenceManagerError          (int errorCode, const QString& errorMsg);
     void _rallyPointManagerError        (int errorCode, const QString& errorMsg);
@@ -1309,7 +1310,37 @@ private:
     uint16_t _lastSetMsgIntervalMsgId = 0;
 
 /*===========================================================================*/
-/*                         Status Text Handler                               */
+/*                         CONTROL STATUS HANDLER                            */
+/*===========================================================================*/
+public:
+    Q_INVOKABLE void requestOperatorControl(bool allowOverride);
+
+private:
+    void _handleControlStatus(const mavlink_message_t& message);
+    void _handleCommandRequestOperatorControl(const mavlink_command_long_t commandLong);
+
+    Q_PROPERTY(uint8_t sysidInControl                        READ sysidInControl                        NOTIFY gcsControlStatusChanged)
+    Q_PROPERTY(bool    gcsControlStatusFlags_SystemManager   READ gcsControlStatusFlags_SystemManager   NOTIFY gcsControlStatusChanged)
+    Q_PROPERTY(bool    gcsControlStatusFlags_TakeoverAllowed READ gcsControlStatusFlags_TakeoverAllowed NOTIFY gcsControlStatusChanged)
+    Q_PROPERTY(bool    firstControlStatusReceived            READ firstControlStatusReceived            NOTIFY gcsControlStatusChanged)
+
+    uint8_t sysidInControl() const { return _sysid_in_control; }
+    bool    gcsControlStatusFlags_SystemManager() const { return _gcsControlStatusFlags_SystemManager; }
+    bool    gcsControlStatusFlags_TakeoverAllowed() const { return _gcsControlStatusFlags_TakeoverAllowed; }
+    bool    firstControlStatusReceived() const { return _firstControlStatusReceived; }
+    
+    uint8_t _sysid_in_control = 0;
+    uint8_t _gcsControlStatusFlags = 0;
+    bool    _gcsControlStatusFlags_SystemManager = 0;
+    bool    _gcsControlStatusFlags_TakeoverAllowed = 0;
+    bool    _firstControlStatusReceived = false;
+
+signals:
+    void gcsControlStatusChanged();
+    void requestOperatorControlReceived(int sysIdRequestingControl, int allowTakeover);
+
+/*===========================================================================*/
+/*                         STATUS TEXT HANDLER                               */
 /*===========================================================================*/
 private:
     Q_PROPERTY(bool    messageTypeNone    READ messageTypeNone    NOTIFY messageTypeChanged)
